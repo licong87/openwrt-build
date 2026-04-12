aa631d87e32f579c5796f483a6de4559d16870b2
licong87 <61625818+licong87@users.noreply.github.com>
2026-04-12 14:24:25 +0800

    Update Settings.sh

--- scripts/Settings.sh
+++ scripts/Settings.sh
@@ -1,40 +1,41 @@
 #!/bin/bash
 
-#配置文件修改
-echo "CONFIG_PACKAGE_luci=y" >> ./.config
-echo "CONFIG_LUCI_LANG_zh_Hans=y" >> ./.config
+# 1. 基础配置修改
+echo "CONFIG_PACKAGE_luci=y" >> .config
+echo "CONFIG_LUCI_LANG_zh_Hans=y" >> .config
 
-# 调整内核参数 /etc/sysctl.conf
+# 2. 调整内核参数 /etc/sysctl.conf (优化 UDP 转发，对网游和科学很有帮助)
 mkdir -p files/etc
 echo "net.netfilter.nf_conntrack_udp_timeout=10" >> files/etc/sysctl.conf
 echo "net.netfilter.nf_conntrack_udp_timeout_stream=60" >> files/etc/sysctl.conf
 
-#高通平台调整
-if [[ $TARGET == *"ipq"* ]]; then
-	#取消nss相关feed
-	echo "CONFIG_FEED_nss_packages=n" >> ./.config
-	echo "CONFIG_FEED_sqm_scripts_nss=n" >> ./.config
-	#设置NSS版本
-	echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> ./.config
-	echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> ./.config
-
-	echo "nss version has fixed!"	
+# 3. 高通平台调整 (精准匹配环境)
+if [ -d "target/linux/qualcommax" ]; then
+    # 取消旧版 NSS，锁定适合大内存机器的 12.2 版本
+    echo "CONFIG_FEED_nss_packages=n" >> .config
+    echo "CONFIG_FEED_sqm_scripts_nss=n" >> .config
+    echo "CONFIG_NSS_FIRMWARE_VERSION_11_4=n" >> .config
+    echo "CONFIG_NSS_FIRMWARE_VERSION_12_2=y" >> .config
+    echo "NSS version has fixed!"    
 fi
 
-# 解除亚瑟内核封印：强行修改内核分区大小为 12M (配合大分区 U-Boot 使用)
-sed -i "/^define Device\/jdcloud_re-ss-01/,/^endef/ { /KERNEL_SIZE := 6144k/s//KERNEL_SIZE := 12288k/ }" target/linux/qualcommax/image/ipq60xx.mk
+# 4. 解除亚瑟内核封印：6M 变 12M (只有你要刷大分区版 U-Boot 才需要这个)
+sed -i "s/KERNEL_SIZE := 6144k/KERNEL_SIZE := 12288k/g" target/linux/qualcommax/image/ipq60xx.mk
 
-# 修改系统默认时区为 亚洲/上海
+# 5. 修改系统默认时区 (解决日志时间对不上的问题)
 sed -i "s/timezone='UTC'/timezone='CST-8'/g" package/base-files/files/bin/config_generate
 sed -i "s/zonename='UTC'/zonename='Asia\/Shanghai'/g" package/base-files/files/bin/config_generate
 
-# 全局基础设施：补齐标准时区数据库 (修复 dae/Nikki 等 Go 程序日志为 UTC 的问题)
+# 6. 补齐标准时区数据库 (必选，否则 dae/Nikki 控制面板显示的时间是乱的)
 echo "CONFIG_PACKAGE_zoneinfo-core=y" >> .config
 echo "CONFIG_PACKAGE_zoneinfo-asia=y" >> .config
 
-# 路径千万别带 openwrt/ 前缀，因为你已经 cd 进去了
+# 7. 强行注入 factory.ubi 生成规则 (注意：这里我用了标准的转义，确保格式正确)
 sed -i '/define Device\/zn_m2/a\  IMAGES += factory.ubi\n  IMAGE/factory.ubi := append-ubi' target/linux/qualcommax/image/ipq60xx.mk
 sed -i '/define Device\/jdcloud_re-ss-01/a\  IMAGES += factory.ubi\n  IMAGE/factory.ubi := append-ubi' target/linux/qualcommax/image/ipq60xx.mk
 
-# 锁定 sing-box 也要用相对路径
+# 8. 锁定 sing-box 版本
 sed -i 's/PKG_VERSION:=.*/PKG_VERSION:=1.12.4/g' feeds/packages/net/sing-box/Makefile
+
+# 9. 针对 x86 的 Nikki 编译补丁 (即使不编 x86 留着也无害，这是最稳的删法)
+find package/ -type f -path "*/nikki/Makefile" -exec sed -i '/define Build\/InstallDev/,/endef/d' {} \;

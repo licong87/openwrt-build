10a060db07cb7db9cd04fa0fb1276ad6b2be0fab
licong87 <61625818+licong87@users.noreply.github.com>
2026-04-14 18:27:04 +0800

    Update Packages.sh

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -21,7 +21,7 @@ UPDATE_PACKAGE() {
 }
 
 UPDATE_PACKAGE "luci-theme-argon" "sbwml/luci-theme-argon" "openwrt-25.12"
-UPDATE_PACKAGE "luci-app-argon-config" "sbwml/luci-app-argon-config" "openwrt-25.12"
+UPDATE_PACKAGE "luci-app-argon-config" "jerrykuku/luci-app-argon-config" "master"
 UPDATE_PACKAGE "aurora" "eamonxg/luci-theme-aurora" "master"
 UPDATE_PACKAGE "aurora-config" "eamonxg/luci-app-aurora-config" "master"
 UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"

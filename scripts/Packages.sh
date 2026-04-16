7ac3efe33afcfe8cdc0c96a58573a0518855d1e2
licong87 <61625818+licong87@users.noreply.github.com>
2026-04-16 21:48:53 +0800

    Update Packages.sh

--- scripts/Packages.sh
+++ scripts/Packages.sh
@@ -27,6 +27,7 @@ UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 
 # UPDATE_PACKAGE "momo" "nikkinikki-org/OpenWrt-momo" "main"
 UPDATE_PACKAGE "nikki" "nikkinikki-org/OpenWrt-nikki" "main"
+UPDATE_PACKAGE "luci-app-dae" "QiuSimons/luci-app-dae" "kix"
 # UPDATE_PACKAGE "openclash" "vernesong/OpenClash" "dev" "pkg"
 # UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
 # UPDATE_PACKAGE "passwall2" "Openwrt-Passwall/openwrt-passwall2" "main" "pkg"
